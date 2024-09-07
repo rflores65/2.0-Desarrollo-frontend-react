db07940bb810cb03b91d56d94ed48f1b71b1839c
rflores65 <ramiro.ibss@gmail.com>
2024-09-06 21:53:24 -0600

    package, app, package-lock

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { act } from 'react'; // Importar 'act' desde 'react'
 import App from './App';
 
 test('renders learn react link', () => {
-  act(() => { // Envolver el render en un act
-    render(<App />);
-  });
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
