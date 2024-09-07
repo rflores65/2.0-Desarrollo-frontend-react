60389a231fc837bdd1dd89d222c2f1b31c1fab43
rflores65 <ramiro.ibss@gmail.com>
2024-09-06 21:29:49 -0600

    config app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { act } from 'react'; // Importar 'act' desde 'react'
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => { // Envolver el render en un act
+    render(<App />);
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
