ce058fc8c17fc7819ac0af6a72136d5a0e52940e
rflores65 <ramiro.ibss@gmail.com>
2024-09-06 23:25:27 -0600

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,6 @@
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    const username = useSelector((state)=>state.form.formData.username)
     return (
         <nav>
             <ul>
